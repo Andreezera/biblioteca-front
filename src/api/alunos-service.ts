b5a098fad0ce32fa46652b61e4359c113c98b5e4
Pedro Leme <32654571+pehleme@users.noreply.github.com>
2024-06-17 21:18:12 -0300

    update endpoints

--- src/api/alunos-service.ts
+++ src/api/alunos-service.ts
@@ -2,23 +2,23 @@ import { api } from "@/lib/axios";
 import { Aluno } from "@/models/aluno";
 
 export async function getAlunos() {
-  const { data } = await api.get<Aluno[]>("/alunos");
+  const { data } = await api.get<Aluno[]>("/clientes/alunos");
   return data;
 }
 
 export async function getAluno(id: number) {
-  const { data } = await api.get<Aluno>(`/alunos/${id}`);
+  const { data } = await api.get<Aluno>(`/clientes/alunos/${id}`);
   return data;
 }
 
 export async function createAluno(body: Aluno) {
-  await api.post("/alunos", body);
+  await api.post("/clientes/alunos", body);
 }
 
 export async function updateAluno(body: Aluno, id: number) {
-  await api.put(`/alunos/${id}`, body);
+  await api.put(`/clientes/alunos/${id}`, body);
 }
 
 export async function deleteAluno(id: number) {
-  await api.delete(`/alunos/${id}`);
+  await api.delete(`/clientes/alunos/${id}`);
 }
--- src/api/professores-service.ts
+++ src/api/professores-service.ts
@@ -2,23 +2,23 @@ import { api } from "@/lib/axios";
 import { Professor } from "@/models/professor";
 
 export async function getProfessores() {
-  const { data } = await api.get<Professor[]>("/professores");
+  const { data } = await api.get<Professor[]>("/clientes/professores");
   return data;
 }
 
 export async function getProfessor(id: number) {
-  const { data } = await api.get<Professor>(`/professores/${id}`);
+  const { data } = await api.get<Professor>(`/clientes/professores/${id}`);
   return data;
 }
 
 export async function createProfessor(body: Professor) {
-  await api.post("/professores", body);
+  await api.post("/clientes/professores", body);
 }
 
 export async function updateProfessor(body: Professor, id: number) {
-  await api.put(`/professores/${id}`, body);
+  await api.put(`/clientes/professores/${id}`, body);
 }
 
 export async function deleteProfessor(id: number) {
-  await api.delete(`/professores/${id}`);
+  await api.delete(`/clientes/professores/${id}`);
 }
